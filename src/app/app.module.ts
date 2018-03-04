d292b4818d32ea3aab5075dd83a3ee8c17e07e06
Jonas Flodén <jonas@koalasoft.se>
2018-03-04 20:08:30 +0100

    Add all components and services to App module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from "@angular/common/http";
 
 
 import { AppComponent } from './app.component';
 
+import { AppRoutingModule } from './/app-routing.module';
+
+import { GenresComponent } from "./genres/genres.component";
+import { MoviesComponent } from "./movies/movies.component";
+import { MoviesListComponent } from "./movies-list/movies-list.component";
+
+import { GenreService } from "./genres/genre.service";
+import { MovieService } from "./movies/movie.service";
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GenresComponent,
+    MoviesComponent,
+    MoviesListComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    AppRoutingModule,
   ],
-  providers: [],
+  providers: [
+    GenreService,
+    MovieService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }

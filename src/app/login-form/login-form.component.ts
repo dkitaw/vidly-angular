5a6b15d7e4718132ddbfec4db2f5b08f8cc3f4c5
Jonas Flodén <jonas@koalasoft.se>
2018-03-05 16:22:01 +0100

    Add login form component

---
+++ src/app/login-form/login-form.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { AuthService } from '../auth/auth.service';
+import { AuthParams } from '../auth/auth-params.model';
+
+@Component({
+  selector: 'app-login-form',
+  templateUrl: './login-form.component.html',
+  styleUrls: ['./login-form.component.css']
+})
+export class LoginFormComponent implements OnInit {
+
+  errorMessage: string;
+
+  constructor(
+    private activeModal: NgbActiveModal,
+    private authService: AuthService
+  ) { }
+
+  ngOnInit() {
+  }
+
+  submit(form) {
+    console.log(form.value);
+    const params = new AuthParams();
+    params.email = form.value['login-email'];
+    params.password = form.value['login-password'];
+    this.authService.login(params).subscribe(message => {
+      if (message) {
+        this.errorMessage = message;
+      } else {
+        this.errorMessage = null;
+        this.activeModal.close();
+      }
+    });
+  }
+
+  diagnostic(form) { return JSON.stringify(form); }
+}

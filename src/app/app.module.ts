a22236e61339683f190d4e6f484ffb3efcc81cf3
Jonas Flodén <jonas@koalasoft.se>
2018-05-13 14:58:54 +0200

    Switch to single quotes in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
-import { FormsModule } from "@angular/forms";
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
-import { NgbModule } from "@ng-bootstrap/ng-bootstrap";
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 
 import { AppRoutingModule } from './/app-routing.module';
 
-import { AuthTokenInterceptor } from "./auth/auth-token.interceptor";
+import { AuthTokenInterceptor } from './auth/auth-token.interceptor';
 
-import { GenresComponent } from "./genres/genres.component";
-import { MoviesComponent } from "./movies/movies.component";
-import { MoviesListComponent } from "./movies-list/movies-list.component";
+import { GenresComponent } from './genres/genres.component';
+import { MoviesComponent } from './movies/movies.component';
+import { MoviesListComponent } from './movies-list/movies-list.component';
 import { LoginFormComponent } from './login-form/login-form.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
-import { AuthService } from "./auth/auth.service";
-import { GenreService } from "./genres/genre.service";
-import { MovieService } from "./movies/movie.service";
+import { AuthService } from './auth/auth.service';
+import { MovieService } from './movies/movie.service';
 import { GenreFormComponent } from './genre-form/genre-form.component';
 import { BootstrapModalComponent } from './bootstrap-modal/bootstrap-modal.component';
 
